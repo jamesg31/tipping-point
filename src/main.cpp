aa83dbf5e5c0ac2c32b9c59302b866af71409c48
James Gardner <jamie.gardner25@gmail.com>
2021-11-19 00:26:32 -0800

    Speed and Brake Control

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,43 @@
 #include "main.h"
+#include <string>
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor left_mtr(2);
+pros::Motor right_mtr(3);
+pros::Motor left_claw(4);
+pros::Motor right_claw(5);
+
+int speed = 2;
+bool locked = true;
+
+void display_fn()
+{
+	master.clear();
+	while (true)
+	{
+		double battery = master.get_battery_capacity();
+		master.set_text(0, 0, std::to_string(battery).substr(0, 2) + "% " + std::to_string(pros::competition::get_status()));
+		pros::delay(50);
+		if (speed == 1)
+		{
+			master.set_text(1, 0, "Sp(X): F");
+		}
+		else
+		{
+			master.set_text(1, 0, "Sp(X): S");
+		}
+		pros::delay(50);
+		if (locked)
+		{
+			master.set_text(1, 9, "Br(A): On ");
+		}
+		else
+		{
+			master.set_text(1, 9, "Br(A): Off");
+		}
+		pros::delay(50);
+	}
+}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -8,7 +47,7 @@
  */
 void initialize()
 {
-	pros::lcd::initialize();
+	pros::Task display_task(display_fn);
 }
 
 /**
@@ -57,22 +96,19 @@ void autonomous() {}
  */
 void opcontrol()
 {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(2);
-	pros::Motor right_mtr(3);
-	pros::Motor left_claw(4);
-	pros::Motor right_claw(5);
-
-	left_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
-	right_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
+	if (locked)
+	{
+		left_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
+		right_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
+	}
 
 	while (true)
 	{
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
+		left_mtr = left / speed;
+		right_mtr = right / speed;
 
 		if (master.get_digital(DIGITAL_L1))
 		{
@@ -99,6 +135,35 @@ void opcontrol()
 		{
 			right_claw.move_velocity(0);
 		}
-		pros::delay(20);
+
+		if (master.get_digital_new_press(DIGITAL_X))
+		{
+			if (speed == 1)
+			{
+				speed = 2;
+			}
+			else
+			{
+				speed = 1;
+			}
+		}
+
+		if (master.get_digital_new_press(DIGITAL_A))
+		{
+			if (locked)
+			{
+				locked = false;
+				left_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_COAST);
+				right_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_COAST);
+			}
+			else
+			{
+				locked = true;
+				left_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
+				right_claw.set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
+			}
+		}
+
+		pros::delay(10);
 	}
 }
